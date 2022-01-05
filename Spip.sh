72849cf3f36fa6be5ab6f69573bf4e6880dca1a5
juugeebee <july2023@yahoo.fr>
2022-01-05 11:07:29 +0100

    up-to-date

--- Spip.sh
+++ Spip.sh
@@ -3,9 +3,8 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate splice
 
-input='/media/jbogoin/Sep2020-JGB/Donnees_brutes/hg19/Metabo/variants_introniques/SPIP/15814_ABCC8intronique_SPIP_input.txt'
-output='/media/jbogoin/Sep2020-JGB/Donnees_brutes/hg19/Metabo/variants_introniques/SPIP/15814_ABCC8intronique_SPIP_output.txt'
-
+input='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SpliceAI.vcf'
+output='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SPIP_output.txt'
 
 cd ~/SPiP/
 Rscript SPiPv2.1_main.r -I $input -O $output
--- SpliceAI.sh
+++ SpliceAI.sh
@@ -6,9 +6,9 @@ conda activate splice
 # cat vcf_header.txt 15920_ABCC8intronique_genomique_vcf_format.csv > 15920_ABCC8intronique_SpliceAI.vcf
 # remplacer les , par des \t
 
-input='/media/jbogoin/Sep2020-JGB/Donnees_brutes/hg19/Metabo/variants_introniques/SpliceAI/15814_ABCC8intronique_SpliceAI.vcf'
-output='/media/jbogoin/Sep2020-JGB/Donnees_brutes/hg19/Metabo/variants_introniques/SpliceAI/15814_ABCC8intronique_SpliceAI_output.vcf'
-genome='/media/Data1/jbogoin/ref/fa_hg19/hg19.fa'
+input='/media/jbogoin/Data1/Variants_introniques/CSM/SpliceAI/variants-pour-bioinfo-20211223_SpliceAI.vcf'
+output='//media/jbogoin/Data1/Variants_introniques/CSM/SpliceAI/variants-pour-bioinfo-20211223_SpliceAI_output.vcf'
+genome='/media/jbogoin/Data1/References/fa_hg19/hg19_std_M-rCRS_Y-PAR-mask.fa'
 
 # -D: Maximum distance between the variant and gained/lost splice site (default: 50)
 # max 4999.
