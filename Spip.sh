af21a8fff2cfc69d2c910a0c8c81ce76c6701c2c
juugeebee <july2023@yahoo.fr>
2022-01-07 15:44:37 +0100

    cmd v1.0

--- Spip.sh
+++ Spip.sh
@@ -4,11 +4,15 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate splice
 
 input='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SpliceAI.vcf'
-output_txt='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SPIP_output_v2.1.txt'
-output_vcf='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SPIP_output_v2.1.vcf'
-
+output_txt='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SPIP_output_v1.0.txt'
+output_vcf='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SPIP_output_v1.0.vcf'
+fasta='/media/jbogoin/Data1/References/fa_hg19/hg19_std_M-rCRS_Y-PAR-mask.fa'
+sam='/usr/bin/samtools'
 
 cd ~/SPiP/
 
 Rscript SPiPv2.1_main.r -I $input -O $output_vcf --VCF
 #Rscript SPiPv2.1_main.r -I $input -O $output_txt
+
+Rscript SPiPv1.0.r -I $input \
+    -O $output_vcf --VCF -f $fasta -s $sam
