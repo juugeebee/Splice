d62bfdbb35e03bdc53cd20d7c3ac16fe039d36b0
juugeebee <july2023@yahoo.fr>
2022-01-07 12:29:29 +0100

    output vcf

--- Spip.sh
+++ Spip.sh
@@ -4,7 +4,11 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate splice
 
 input='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SpliceAI.vcf'
-output='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SPIP_output.txt'
+output_txt='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SPIP_output_v2.1.txt'
+output_vcf='/media/jbogoin/Data1/Variants_introniques/CSM_05012022/SPIP/variants-pour-bioinfo-20211223_SPIP_output_v2.1.vcf'
+
 
 cd ~/SPiP/
-Rscript SPiPv2.1_main.r -I $input -O $output
+
+Rscript SPiPv2.1_main.r -I $input -O $output_vcf --VCF
+#Rscript SPiPv2.1_main.r -I $input -O $output_txt
